d592c0f287f35be4c2011958c7f2f99eb192f2ed
Yunshi-Liang <114257224+Yunshi-Liang@users.noreply.github.com>
2023-01-06 21:05:12 +0100

    merged map page

---
+++ data/Management.js
@@ -0,0 +1,556 @@
+var Management ={
+  "type" : "FeatureCollection",
+  "features" : [
+    {
+      "type" : "Feature",
+      "id" : 24,
+      "geometry" : {
+        "type" : "Point",
+        "coordinates" : [
+          -3.2479718181648778,
+          55.933080011960406
+        ]
+      },
+      "properties" : {
+        "OBJECTID" : 24,
+        "Note" : "Big Barrier",
+        "passability" : "no",
+        "X" : 322130.70870000031,
+        "Y" : 671817.04790000059,
+        "Source" : "SEPA"
+      }
+    },
+    {
+      "type" : "Feature",
+      "id" : 25,
+      "geometry" : {
+        "type" : "Point",
+        "coordinates" : [
+          -3.2284037860468144,
+          55.949563011927452
+        ]
+      },
+      "properties" : {
+        "OBJECTID" : 25,
+        "Note" : "Big Barrier",
+        "passability" : "no",
+        "X" : 323385.79009999987,
+        "Y" : 673629.57569999993,
+        "Source" : "SEPA"
+      }
+    },
+    {
+      "type" : "Feature",
+      "id" : 26,
+      "geometry" : {
+        "type" : "Point",
+        "coordinates" : [
+          -3.2219237938968925,
+          55.950601020587698
+        ]
+      },
+      "properties" : {
+        "OBJECTID" : 26,
+        "Note" : "Small Barrier",
+        "passability" : "yes",
+        "X" : 323792.4796000002,
+        "Y" : 673737.92679999955,
+        "Source" : "SEPA"
+      }
+    },
+    {
+      "type" : "Feature",
+      "id" : 27,
+      "geometry" : {
+        "type" : "Point",
+        "coordinates" : [
+          -3.219827794607188,
+          55.952401023541434
+        ]
+      },
+      "properties" : {
+        "OBJECTID" : 27,
+        "Note" : "Big Barrier",
+        "passability" : "no",
+        "X" : 323926.89719999954,
+        "Y" : 673935.94669999927,
+        "Source" : "SEPA"
+      }
+    },
+    {
+      "type" : "Feature",
+      "id" : 29,
+      "geometry" : {
+        "type" : "Point",
+        "coordinates" : [
+          -3.216008841057755,
+          55.952967011543635
+        ]
+      },
+      "properties" : {
+        "OBJECTID" : 29,
+        "Note" : "Big Barrier",
+        "passability" : "no",
+        "X" : 324166.46910000034,
+        "Y" : 673994.73819999956,
+        "Source" : "SEPA"
+      }
+    },
+    {
+      "type" : "Feature",
+      "id" : 30,
+      "geometry" : {
+        "type" : "Point",
+        "coordinates" : [
+          -3.1899188320156227,
+          55.969018017359055
+        ]
+      },
+      "properties" : {
+        "OBJECTID" : 30,
+        "Note" : "Small Barrier",
+        "passability" : "yes",
+        "X" : 325826.3331000004,
+        "Y" : 675752.81469999999,
+        "Source" : "SEPA"
+      }
+    },
+    {
+      "type" : "Feature",
+      "id" : 31,
+      "geometry" : {
+        "type" : "Point",
+        "coordinates" : [
+          -3.2590068191749144,
+          55.925934031653092
+        ]
+      },
+      "properties" : {
+        "OBJECTID" : 31,
+        "Note" : "Wall Collapse",
+        "passability" : "",
+        "X" : 321426.79910000041,
+        "Y" : 671034.34840000048,
+        "Source" : "FIELD"
+      }
+    },
+    {
+      "type" : "Feature",
+      "id" : 32,
+      "geometry" : {
+        "type" : "Point",
+        "coordinates" : [
+          -3.2586428204823736,
+          55.926105030707909
+        ]
+      },
+      "properties" : {
+        "OBJECTID" : 32,
+        "Note" : "Island",
+        "passability" : null,
+        "X" : 321449.89030000009,
+        "Y" : 671052.96319999918,
+        "Source" : "FIELD"
+      }
+    },
+    {
+      "type" : "Feature",
+      "id" : 33,
+      "geometry" : {
+        "type" : "Point",
+        "coordinates" : [
+          -3.1989078233800394,
+          55.964046018337065
+        ]
+      },
+      "properties" : {
+        "OBJECTID" : 33,
+        "Note" : "Flood Gate",
+        "passability" : null,
+        "X" : 325255.67030000035,
+        "Y" : 675209.14130000025,
+        "Source" : "SEPA"
+      }
+    },
+    {
+      "type" : "Feature",
+      "id" : 34,
+      "geometry" : {
+        "type" : "Point",
+        "coordinates" : [
+          -3.2079374461306767,
+          55.959024710282293
+        ]
+      },
+      "properties" : {
+        "OBJECTID" : 34,
+        "Note" : "Flood Gate",
+        "passability" : null,
+        "X" : 324682.24309999961,
+        "Y" : 674660.10190000013,
+        "Source" : "SEPA"
+      }
+    },
+    {
+      "type" : "Feature",
+      "id" : 35,
+      "geometry" : {
+        "type" : "Point",
+        "coordinates" : [
+          -3.3199948106382009,
+          55.892462024029072
+        ]
+      },
+      "properties" : {
+        "OBJECTID" : 35,
+        "Note" : "New Housing",
+        "passability" : null,
+        "X" : 317544.94249999989,
+        "Y" : 667380.33770000003,
+        "Source" : "FIELD"
+      }
+    },
+    {
+      "type" : "Feature",
+      "id" : 39,
+      "geometry" : {
+        "type" : "Point",
+        "coordinates" : [
+          -3.3032738139025719,
+          55.896634027067286
+        ]
+      },
+      "properties" : {
+        "OBJECTID" : 39,
+        "Note" : "Big Barrier",
+        "passability" : null,
+        "X" : 318599.38179999962,
+        "Y" : 667824.8088000007,
+        "Source" : "SEPA"
+      }
+    },
+    {
+      "type" : "Feature",
+      "id" : 42,
+      "geometry" : {
+        "type" : "Point",
+        "coordinates" : [
+          -3.2957988055126219,
+          55.898830025686877
+        ]
+      },
+      "properties" : {
+        "OBJECTID" : 42,
+        "Note" : "New Housing",
+        "passability" : null,
+        "X" : 319071.37770000007,
+        "Y" : 668060.4196000006,
+        "Source" : "FIELD"
+      }
+    },
+    {
+      "type" : "Feature",
+      "id" : 44,
+      "geometry" : {
+        "type" : "Point",
+        "coordinates" : [
+          -3.2950028062644456,
+          55.899064025394686
+        ]
+      },
+      "properties" : {
+        "OBJECTID" : 44,
+        "Note" : "Island",
+        "passability" : null,
+        "X" : 319121.63680000044,
+        "Y" : 668085.52930000052,
+        "Source" : "FIELD"
+      }
+    },
+    {
+      "type" : "Feature",
+      "id" : 45,
+      "geometry" : {
+        "type" : "Point",
+        "coordinates" : [
+          -3.2776588086415304,
+          55.904425025135716
+        ]
+      },
+      "properties" : {
+        "OBJECTID" : 45,
+        "Note" : "New Housing",
+        "passability" : null,
+        "X" : 320217.12390000001,
+        "Y" : 668661.99019999988,
+        "Source" : "FIELD"
+      }
+    },
+    {
+      "type" : "Feature",
+      "id" : 46,
+      "geometry" : {
+        "type" : "Point",
+        "coordinates" : [
+          -3.2751608105060761,
+          55.905943024769918
+        ]
+      },
+      "properties" : {
+        "OBJECTID" : 46,
+        "Note" : "Big Barrier",
+        "passability" : null,
+        "X" : 320376.40950000007,
+        "Y" : 668828.04729999974,
+        "Source" : "SEPA"
+      }
+    },
+    {
+      "type" : "Feature",
+      "id" : 47,
+      "geometry" : {
+        "type" : "Point",
+        "coordinates" : [
+          -3.2591748045533109,
+          55.911522023727919
+        ]
+      },
+      "properties" : {
+        "OBJECTID" : 47,
+        "Note" : "Big Barrier",
+        "passability" : "no",
+        "X" : 321387.08739999961,
+        "Y" : 669430.62340000086,
+        "Source" : "SEPA"
+      }
+    },
+    {
+      "type" : "Feature",
+      "id" : 49,
+      "geometry" : {
+        "type" : "Point",
+        "coordinates" : [
+          -3.2477528187097748,
+          55.922944029551317
+        ]
+      },
+      "properties" : {
+        "OBJECTID" : 49,
+        "Note" : "New Housing",
+        "passability" : null,
+        "X" : 322123.97059999965,
+        "Y" : 670688.84190000035,
+        "Source" : "FIELD"
+      }
+    },
+    {
+      "type" : "Feature",
+      "id" : 50,
+      "geometry" : {
+        "type" : "Point",
+        "coordinates" : [
+          -3.2625585174159681,
+          55.905820903947422
+        ]
+      },
+      "properties" : {
+        "OBJECTID" : 50,
+        "Note" : "Big Barrier",
+        "passability" : "no",
+        "X" : 321164,
+        "Y" : 668800,
+        "Source" : "SEPA"
+      }
+    },
+    {
+      "type" : "Feature",
+      "id" : 51,
+      "geometry" : {
+        "type" : "Point",
+        "coordinates" : [
+          -3.2796367364294179,
+          55.904197904489365
+        ]
+      },
+      "properties" : {
+        "OBJECTID" : 51,
+        "Note" : "Small Barrier",
+        "passability" : "yes",
+        "X" : 320093,
+        "Y" : 668639,
+        "Source" : "SEPA"
+      }
+    },
+    {
+      "type" : "Feature",
+      "id" : 52,
+      "geometry" : {
+        "type" : "Point",
+        "coordinates" : [
+          -3.2908949521985451,
+          55.900562744697218
+        ]
+      },
+      "properties" : {
+        "OBJECTID" : 52,
+        "Note" : "Big Barrier",
+        "passability" : "no",
+        "X" : 319381.60080000013,
+        "Y" : 668247.52089999989,
+        "Source" : "SEPA"
+      }
+    },
+    {
+      "type" : "Feature",
+      "id" : 53,
+      "geometry" : {
+        "type" : "Point",
+        "coordinates" : [
+          -3.2934130956313457,
+          55.900200684099872
+        ]
+      },
+      "properties" : {
+        "OBJECTID" : 53,
+        "Note" : "Small Barrier",
+        "passability" : "yes",
+        "X" : 319223.40189999994,
+        "Y" : 668210.16840000078,
+        "Source" : "SEPA"
+      }
+    },
+    {
+      "type" : "Feature",
+      "id" : 54,
+      "geometry" : {
+        "type" : "Point",
+        "coordinates" : [
+          -3.3126400254691997,
+          55.894406643790447
+        ]
+      },
+      "properties" : {
+        "OBJECTID" : 54,
+        "Note" : "Small Barrier",
+        "passability" : "yes",
+        "X" : 318009,
+        "Y" : 667588,
+        "Source" : "SEPA"
+      }
+    },
+    {
+      "type" : "Feature",
+      "id" : 55,
+      "geometry" : {
+        "type" : "Point",
+        "coordinates" : [
+          -3.323388416718728,
+          55.890248037726103
+        ]
+      },
+      "properties" : {
+        "OBJECTID" : 55,
+        "Note" : "Small Barrier",
+        "passability" : "yes",
+        "X" : 317328,
+        "Y" : 667138,
+        "Source" : "SEPA"
+      }
+    },
+    {
+      "type" : "Feature",
+      "id" : 56,
+      "geometry" : {
+        "type" : "Point",
+        "coordinates" : [
+          -3.3179697329408611,
+          55.91293174466287
+        ]
+      },
+      "properties" : {
+        "OBJECTID" : 56,
+        "Note" : "Small Barrier",
+        "passability" : "yes",
+        "X" : 317715,
+        "Y" : 669656,
+        "Source" : "SEPA"
+      }
+    },
+    {
+      "type" : "Feature",
+      "id" : 57,
+      "geometry" : {
+        "type" : "Point",
+        "coordinates" : [
+          -3.2484953400357028,
+          55.92265937113816
+        ]
+      },
+      "properties" : {
+        "OBJECTID" : 57,
+        "Note" : "Small Barrier",
+        "passability" : "yes",
+        "X" : 322077,
+        "Y" : 670658,
+        "Source" : "SEPA"
+      }
+    },
+    {
+      "type" : "Feature",
+      "id" : 58,
+      "geometry" : {
+        "type" : "Point",
+        "coordinates" : [
+          -3.317028623663373,
+          55.865434368505007
+        ]
+      },
+      "properties" : {
+        "OBJECTID" : 58,
+        "Note" : "Dam",
+        "passability" : "no",
+        "X" : 317673.17640000023,
+        "Y" : 664368.91039999947,
+        "Source" : "FIELD"
+      }
+    },
+    {
+      "type" : "Feature",
+      "id" : 59,
+      "geometry" : {
+        "type" : "Point",
+        "coordinates" : [
+          -3.3113758477552859,
+          55.872522150871951
+        ]
+      },
+      "properties" : {
+        "OBJECTID" : 59,
+        "Note" : "Dam",
+        "passability" : "no",
+        "X" : 318041.88439999986,
+        "Y" : 665150.98509999923,
+        "Source" : "FIELD"
+      }
+    },
+    {
+      "type" : "Feature",
+      "id" : 60,
+      "geometry" : {
+        "type" : "Point",
+        "coordinates" : [
+          -3.4339377421687778,
+          55.835703331572397
+        ]
+      },
+      "properties" : {
+        "OBJECTID" : 60,
+        "Note" : "Dam",
+        "passability" : "no",
+        "X" : 310288.37370000035,
+        "Y" : 661205.66159999929,
+        "Source" : "FIELD"
+      }
+    }
+  ]
+}
